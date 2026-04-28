d98a9f91f0f054a512f488f7eea767cf3e883283
SARANYAN V <150230865+saranyan123@users.noreply.github.com>
2026-04-28 13:48:55 +0530

    Time: 1342 ms (14.23%) | Memory: 0B (100.00%) - LeetSync

---
+++ 185-department-top-three-salaries/department-top-three-salaries.sql
@@ -0,0 +1,13 @@
+SELECT 
+    d.Name AS Department, 
+    e.Name AS Employee, 
+    e.Salary
+FROM 
+    Employee e
+JOIN 
+    Department d ON e.DepartmentId = d.Id
+WHERE 
+    (SELECT COUNT(DISTINCT e2.Salary) 
+     FROM Employee e2 
+     WHERE e2.Salary > e.Salary 
+     AND e2.DepartmentId = e.DepartmentId) < 3;
